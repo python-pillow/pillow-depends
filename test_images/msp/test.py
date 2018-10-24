b22609ac808438852b975d1c928e6a93e5750d5b
Hugo <hugovk@users.noreply.github.com>
2018-10-24 11:48:52 +0300

    Fix flake8

--- test_images/msp/test.py
+++ test_images/msp/test.py
@@ -4,26 +4,28 @@ from PIL import Image, ImageFile
 
 ImageFile.LOAD_TRUNCATED_IMAGES = False
 
-files = ['BLK.MSP',
-         'BLU.MSP',
-         'FLAG_B24.MSP',
-         'GRN.MSP',
-         'MARBLES.MSP',
-         'RED.MSP',
-         'TRU256.MSP',
-         'VENUS.MSP',
-         'WHT.MSP',
-         'YEL.MSP'
-         ]
+files = [
+    "BLK.MSP",
+    "BLU.MSP",
+    "FLAG_B24.MSP",
+    "GRN.MSP",
+    "MARBLES.MSP",
+    "RED.MSP",
+    "TRU256.MSP",
+    "VENUS.MSP",
+    "WHT.MSP",
+    "YEL.MSP",
+]
+
 
 def test(f):
     try:
         im = Image.open(f)
         im.show()
-        im.save(f.replace('.MSP','.png'), format='PNG')
-    except Exception as msg:
-        print ("Could not open %s" %f)
-   
+        im.save(f.replace(".MSP", ".png"), format="PNG")
+    except Exception:
+        print("Could not open %s" % f)
+
 
 for f in files:
     test(f)
--- test_images/picins/test.py
+++ test_images/picins/test.py
@@ -4,21 +4,17 @@ from PIL import Image, ImageFile
 
 ImageFile.LOAD_TRUNCATED_IMAGES = False
 
-files = [ 'mandel.msp',
-          'mexhat1.msp',
-          'mexhat2.msp',
-          ]
+files = ["mandel.msp", "mexhat1.msp", "mexhat2.msp"]
 
 
- 
 def test(f):
     try:
         im = Image.open(f)
         im.show()
-        im.save(f.replace('.msp','.png'), format='PNG')
-    except Exception as msg:
-        print ("Could not open %s" %f)
-   
+        im.save(f.replace(".msp", ".png"), format="PNG")
+    except Exception:
+        print("Could not open %s" % f)
+
 
 for f in files:
     test(f)
